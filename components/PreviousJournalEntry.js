f57b0c92df0dd20ed1065d962e4691139b5c4f08
Rikki Soriano <rikkilanz@gmail.com>
2023-03-14 22:21:40 -0700

    Update PreviousJournalEntry.js

--- components/PreviousJournalEntry.js
+++ components/PreviousJournalEntry.js
@@ -4,7 +4,7 @@ export default function PreviousJournalEntry({data}){
     console.log({data})
     return(
         <View className="bg-black rounded-lg my h-32 p-8">
-            <Text className="text-white">{data.quote}</Text>
+            {/* <Text className="text-white">{data.quote}</Text> */}
             <Text className="text-white">Quote</Text>
             <Text className="text-white">Entry</Text>
             <Text className="text-white">Date</Text>
